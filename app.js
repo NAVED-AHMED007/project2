d3aabb1aa15c4d8eb4475427a543a83e257d9d2f
Naved Ahmed <knaved6785@gmail.com>
2025-08-03 11:42:31 +0530

    Add New Form

---
+++ app.js
@@ -0,0 +1,37 @@
+// Add new feature - form
+
+// --------------------------
+
+// Merging code - 
+// git diff <-branch name-> (to commits ,branchs,files and more)
+// git merge <-branch name-> (to merge 2 branches) OR Create a PR 
+
+// ------------------------
+// Pull Request - it lets you tell other about changes you push to a branch in a repository of github.
+// -------------
+// Basic command - 
+// git pull origin main 
+
+// used to fetch and download content from a remote repo and immediately update the 
+// local repo to match the content.
+
+// ----------------------
+
+// merge conflict - An event that take place when git is unable to automatically resolve 
+// differences in code between two commits.
+
+// ----------------------------
+// Branch Command -
+// git branch (to check branch)
+
+// git branch -M main (to rename branch)
+
+// git checkout <- branch name-> (to navigate)
+
+// git checkout -b <- new branch name -> (to create new branch)
+
+// git branch -d <-branch name-> (to delete branch)
+
+// --------------------------
+
+
