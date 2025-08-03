645c1ec68d04c2bfad042470555ef33fee6c1584
Naved Ahmed <knaved6785@gmail.com>
2025-08-03 11:20:29 +0530

    add new button

---
+++ app.js
@@ -0,0 +1,14 @@
+// Branch Command -
+// git branch (to check branch)
+
+// git branch -M main (to rename branch)
+
+// git checkout <- branch name-> (to navigate)
+
+// git checkout -b <- new branch name -> (to create new branch)
+
+// git branch -d <-branch name-> (to delete branch)
+
+// --------------------------
+
+// Add new feature - button
